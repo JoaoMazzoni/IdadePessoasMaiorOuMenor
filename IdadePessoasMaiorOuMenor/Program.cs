3d2520b9e28b4e5c7deb2871f4c1c1ea5e9e9210
JoaoMazzoni <joao.mazzoni@5by5.com>
2024-04-19 16:37:14 -0300

    Add project files.

---
+++ IdadePessoasMaiorOuMenor/Program.cs
@@ -0,0 +1,34 @@
+﻿int idade;
+int maior = 0;
+int menor = 0;
+
+for(int i = 1; i <= 10; i++)
+{
+    do
+    {
+        Console.Write("\nDigite a idade da Pessoa {0}: ", i);
+        idade = int.Parse(Console.ReadLine());
+        if (idade < 0)
+        {
+            Console.WriteLine("\n|A idade não pode ser menor que zero!\n");
+            Console.WriteLine("------------------------------------------------------");
+        }
+
+    } while (idade < 0);
+   
+    if(idade >= 18)
+    {
+        maior++;
+    }
+
+    else
+    {
+        menor++;
+    }
+
+}
+ 
+Console.WriteLine("\n|Pessoas maiores de idade: {0}", maior);
+Console.WriteLine("|Pessoas menores de idade: {0}", menor);
+
+
